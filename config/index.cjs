29af5308e74b2b9f280f2421dff964eb7dbfe7fc
yolo <mingw0410@163.com>
2022-12-13 05:01:23 +0800

    1234

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1LKO52aCg9dHYuTOUnyRlIiAt1k',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '75XLMUQzP8fN2Aen0RsrLlnWvhbLDa_dXsNVUHjL1t4',
+      useTemplateId: 'Wd-yTAPyYM-DLWVsqipj-0ykb8cw-P4PNJn9rjz9pr4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
